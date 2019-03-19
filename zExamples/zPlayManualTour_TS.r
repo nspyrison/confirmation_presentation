e86b43224d7730841549cfaa590f01e7b3343e45
Nicholas Spyrison <spyrison@gmail.com>
2019-03-19 15:54:05 +1100

    slide revision, gganimate still looks off.

--- zExamples/zPlayManualTour_TS.r
+++ zExamples/zPlayManualTour_TS.r
@@ -9,12 +9,9 @@ f_bas  <- matrix(f_path[,, max(dim(f_path)[3])], ncol=2)
 f_mvar <- 5
 f_proj <- data.frame(tourr::rescale(f_dat %*% f_bas))
 
-# view_basis(f_bas, labels = colnames(f_dat)) +
-#   geom_point(data = f_proj,
-#              mapping = aes(x = X1 - 1.75, y = X2 - .5, color = f_cat),
-#              pch = as.integer(f_cat) + 15)
-
-f_ang <- .35
+f_ang <- .08
 f_mt <- manual_tour(basis = f_bas,manip_var = f_mvar,angle = f_ang)
 
-array2facet(.m_tour = f_mt, .data = f_dat, .m_var = f_mvar, .cat = f_cat)
+play_manual_tour(data = f_dat, basis = f_bas, manip_var = f_mvar,angle = f_ang)
+undebug(manual_tour)
+manual_tour
